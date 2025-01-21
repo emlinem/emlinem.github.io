1f3ad8809099f6c92e8680e67462009c524f421b
emlinem <emily.r.nilsson@gmail.com>
2025-01-21 13:34:24 +0100

    feat: update start page

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
